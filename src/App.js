a1a85eb17844d917b764d450a536cbba7f15d018
lenoxpc <lenoxemperado@gmail.com>
2023-11-16 17:04:16 +0800

    5th commit changes --add assets and font setups

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="text-4xl font-semibold font-signature">Hello World!</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      signature: ['Archivo Black'],
+    }
   },
   plugins: [require("tw-elements/dist/plugin.cjs")],
   darkMode: "class"
-}
+};
